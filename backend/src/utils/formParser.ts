2f7a89d821eb7913812590dd889039cdfc2dbc5e
Fabrizio Silvestri <fabri.silve.fs@gmail.com>
2024-05-01 10:28:16 +0200

    Make explicit the returned form data type from parser

--- backend/src/utils/formParser.ts
+++ backend/src/utils/formParser.ts
@@ -1,8 +1,13 @@
 import { IncomingMessage } from "node:http";
-import formidable from "formidable";
+import formidable, { Fields, Files } from "formidable";
+
+interface IFormData {
+  fields: Fields;
+  files: Files;
+}
 
 const formParser = async (req: IncomingMessage) => {
-  return new Promise((resolve, reject) => {
+  return new Promise<IFormData>((resolve, reject) => {
     const form = new formidable.IncomingForm();
     form.parse(req, (err, fields, files) => {
       if (err) {
