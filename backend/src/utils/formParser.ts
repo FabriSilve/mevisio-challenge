d5c64faa1e9e19ccf0ae9e264cae56d95c5a2e95
Fabrizio Silvestri <fabri.silve.fs@gmail.com>
2024-05-01 12:04:12 +0200

    Wrap from parsing logic in try catch to handle errors

--- backend/src/utils/formParser.ts
+++ backend/src/utils/formParser.ts
@@ -1,5 +1,5 @@
 import { IncomingMessage } from "node:http";
-import formidable, { Fields, Files } from "formidable";
+import { IncomingForm, Fields, Files } from "formidable";
 
 interface IFormData {
   fields: Fields;
@@ -8,14 +8,18 @@ interface IFormData {
 
 const formParser = async (req: IncomingMessage) => {
   return new Promise<IFormData>((resolve, reject) => {
-    const form = new formidable.IncomingForm();
-    form.parse(req, (err, fields, files) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve({ fields, files });
-      }
-    });
+    try {
+      const form = new IncomingForm();
+      form.parse(req, (err, fields, files) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve({ fields, files });
+        }
+      });
+    } catch(e) {
+      reject(e);
+    }
   });
 }
 
